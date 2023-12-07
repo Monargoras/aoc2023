780a61db7a46608124d562cba4d294d5461abdf3
Felix Wagner <felixwagner99@googlemail.com>
2023-12-07 07:46:44 +0100

    day7 skeleton

---
+++ aocDay7/aocDay7.py
@@ -0,0 +1,15 @@
+import re
+
+
+def partOne():
+    with open('exampleInput.txt') as input:
+        pass
+
+
+def partTwo():
+    with open('exampleIinput.txt') as input:
+        pass
+
+
+if __name__ == '__main__':
+    partTwo()
